d314918022c12de008955a98a21a68b830928574
Maxime Mikotajewski <mmikotajewski@witekio.com>
2019-01-07 17:55:06 +0100

    Added a second test

--- turbo-broccoli/ReturnCoucouBrocoli/test/testreturncoucoubrocoli.cpp
+++ turbo-broccoli/ReturnCoucouBrocoli/test/testreturncoucoubrocoli.cpp
@@ -1,10 +1,14 @@
-//#include <gtest/gtest.h>
-
 #include <gtest/gtest.h>
 #include <ReturnCoucouBrocoli/CoucouBrocoliReturner.h>
 
-TEST(returnCoucouBrocoliTest, myTest) {
+TEST(returnCoucouBrocoliTest, testEquals) {
     CoucouBrocoliReturner returner;
     auto str = returner.returnCoucouBrocoli();
     EXPECT_EQ(str, "Coucou turbo brocoli !");
+}
+
+TEST(returnCoucouBrocoliTest, testNotEquals) {
+    CoucouBrocoliReturner returner;
+    auto str = returner.returnCoucouBrocoli();
+    EXPECT_NE(str, "Coucou turboo brocola !");
 }
